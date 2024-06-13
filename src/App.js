a53167115bc9ae02ebb277290fe4416a00256efd
Christian <kristian2015ok@gmail.com>
2024-06-13 14:47:23 +0700

    Perf: Shuffling Cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+  const [card, setCard] = useState([]);
+  const [turns, setTurns] = useState([0]);
+
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCard(shuffleCards);
+    setTurns(0);
+  }
+
+  console.log(card, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
